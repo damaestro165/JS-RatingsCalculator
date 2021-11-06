44c28b34e48c56e2daa6c66ec82dc9efe2efcdaf
damaestro165 <damaestro165@gmail.com>
2021-11-06 17:49:37 +0100

    Completed module

---
+++ js/ratings.js
@@ -0,0 +1,13 @@
+function collect_ratings(){
+    let ratings = {
+        count: 0,
+        sum: 0,
+        average: 0
+    }
+    let rating = 0;
+    const elements = document.querySelector(".rating");
+    elements.forEach((element) => {
+        
+    }
+    );
+}
